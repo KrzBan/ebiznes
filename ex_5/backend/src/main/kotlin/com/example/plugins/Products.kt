3e1f1036c85433ab7c758412fd11e47a7c484cbb
Krzysztof Bański <krzys0100@wp.pl>
2023-05-30 15:48:30 +0200

    ex_7(sonar): added ktlint, fixed warnings, included backend project in sonar files, ktlint execution in git pre-commit hook

--- ex_5/backend/src/main/kotlin/com/example/plugins/Products.kt
+++ ex_5/backend/src/main/kotlin/com/example/plugins/Products.kt
@@ -1,13 +1,13 @@
 package com.example.plugins
 
 import io.ktor.http.*
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
-class Product(var name: String, var price: Double) {
-}
+class Product(var name: String, var price: Double)
+
 fun Application.configureProducts() {
     install(CORS) {
         allowMethod(HttpMethod.Options)
@@ -18,11 +18,15 @@ fun Application.configureProducts() {
         anyHost()
     }
 
-    val products = listOf(Product("Product #1", 10.0), Product("Product #2", 12.0),
-        Product("Product #3", 20.0), Product("Product #4", 50.0))
+    val products = listOf(
+        Product("Product #1", 10.0),
+        Product("Product #2", 12.0),
+        Product("Product #3", 20.0),
+        Product("Product #4", 50.0),
+    )
     routing {
         get("/products") {
-            call.respond( products )
+            call.respond(products)
         }
     }
 }
